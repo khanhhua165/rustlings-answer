2e212e4d3f780f30356950001fc4be106ab52419
khanhhua165 <billiejoe165@gmail.com>
2022-07-15 10:40:58 +0700

    finished

---
+++ clippy/target/debug/deps/clippy2-17fd8f8e27b5b2a3.d
@@ -0,0 +1,7 @@
+/home/huakhanh/rustlings/exercises/clippy/target/debug/deps/clippy2-17fd8f8e27b5b2a3.rmeta: clippy2.rs
+
+/home/huakhanh/rustlings/exercises/clippy/target/debug/deps/clippy2-17fd8f8e27b5b2a3.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
